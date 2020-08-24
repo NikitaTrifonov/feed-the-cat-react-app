ee4a4cc8b1558e4c8a0bd5e34fcd957e34eed847
Nikita <trif430775@gmail.com>
2020-08-24 19:00:34 +0300

    Updates

---
+++ precache-manifest.1dee0af3b41664c166d72060f67344bd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b9e37a44385c7f4b1c4297a9c1b590",
+    "url": "/feed-the-cat-react-app/index.html"
+  },
+  {
+    "revision": "cafa622f209781b3dd4a",
+    "url": "/feed-the-cat-react-app/static/css/main.4112f00b.chunk.css"
+  },
+  {
+    "revision": "4c8f2f2435ede73d2166",
+    "url": "/feed-the-cat-react-app/static/js/2.2dec9015.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/feed-the-cat-react-app/static/js/2.2dec9015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafa622f209781b3dd4a",
+    "url": "/feed-the-cat-react-app/static/js/main.767e7d34.chunk.js"
+  },
+  {
+    "revision": "6f4409784bfcbe490e0b",
+    "url": "/feed-the-cat-react-app/static/js/runtime-main.0e6df6cf.js"
+  },
+  {
+    "revision": "1717a88a2f0e0f7988c3963cfaf40ed2",
+    "url": "/feed-the-cat-react-app/static/media/background.1717a88a.png"
+  },
+  {
+    "revision": "9c44a2b92a7299df245229d9f2b7f75c",
+    "url": "/feed-the-cat-react-app/static/media/defaultProductBackHover.9c44a2b9.png"
+  },
+  {
+    "revision": "41f2356921b3c52fecba6c8e80983a9f",
+    "url": "/feed-the-cat-react-app/static/media/defaultProductMouseOut.41f23569.png"
+  },
+  {
+    "revision": "b62a46835e70acb857859cdfb7855bb9",
+    "url": "/feed-the-cat-react-app/static/media/productIsOver.b62a4683.png"
+  },
+  {
+    "revision": "20b720f3d9078843fa1befd7c59d3225",
+    "url": "/feed-the-cat-react-app/static/media/selectedMouseOut.20b720f3.png"
+  },
+  {
+    "revision": "3248ad64854f4e1719da93f172137d2e",
+    "url": "/feed-the-cat-react-app/static/media/selectedProductHover.3248ad64.png"
+  }
+]);
